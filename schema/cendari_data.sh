5959ecfc26228283c58b1a6cf307e530186e71e0
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2014-07-27 22:45:30 +0200

    add length and create schema

---
+++ schema/cendari_data.sh
@@ -0,0 +1,1 @@
+curl -XPUT 'http://localhost:9200/cendari/document/1?pretty=1' -d '@cendari_data.json'
---
+++ schema/cendari_mapping.sh
@@ -0,0 +1,1 @@
+curl -XPUT 'http://localhost:9200/cendari/document/_mapping?pretty=1' -d '@cendari_mapping.json'
